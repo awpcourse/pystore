42fd4d018d12bcfb3335d4ab7ac3ae35011e195a
Emanuel9 <emanuel.dumitru9@gmail.com>
2016-01-16 12:47:13 +0200

    Login

--- pystore/pystoreapp/urls.py
+++ pystore/pystoreapp/urls.py
@@ -3,13 +3,12 @@ from django.conf.urls import url
 from pystoreapp import views
 
 urlpatterns = [
-    url(r'^$', views.homeView, name='index'),
+    url(r'^$', views.home, name='index'),
     # url(r'^cart/$', '', name='cart'),
     # url(r'^products/$', '', name='products'),
     # url(r'^products/(?P<pk>\d+)$', '', name='products_details'),
     # url(r'^checkout/$', '' , name='checkout'),
     #
-    # url(r'^login/$', '' , name='login'),
     # url(r'^register/$', '', name='register'),
     # url(r'^logout/$', '' , name='logout'),
     # url(r'^userpanel/(?P<pk>\d+)$', '' , name='userpanel'),
--- pystore/pystoreapp/views.py
+++ pystore/pystoreapp/views.py
@@ -5,12 +5,40 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView, DeleteView
 
+from pystoreapp.models import UserProfile
+from pystoreapp.forms import UserLoginForm
 
-def homeView(request):
+def home(request):
     if request.method == 'GET':
-        nume_var = "Mama are mere"
-        context = {'nume' : nume_var}
+        form = UserLoginForm()
+        context = {
+            'form': form,
+        }
         return render(request, 'index.html', context)
+
+    elif request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is None:
+            context = {
+                'form': form,
+                'message': 'Wrong username or password!'
+            }
+        else:
+            login(request, user)
+
+        return redirect('index')
+
+@login_required
+def profileDetails(request, pk):
+    profile = UserProfile.objects.get(pk=pk)
+    return redirect('profile', pk=pk)
+
+
+
+
 
 
 
